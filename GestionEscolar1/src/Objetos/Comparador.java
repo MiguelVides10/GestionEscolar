682e4bc89e2625c9d012058d98ac0ea4afbacf6e
MiguelVides10 <99452681+MiguelVides10@users.noreply.github.com>
2022-05-15 10:25:48 -0600

    Add files via upload

---
+++ GestionEscolar1/src/Objetos/Comparador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author Gilberto Alexander Ventura Hernández
+ *         Miguel Angel Vides Deras
+ */
+public interface Comparador {
+    boolean igualQue(Object r);
+    
+    boolean menorQue(Object r);
+    
+    boolean menorIgual(Object r);
+    
+    boolean mayorQue(Object r);
+    
+    boolean mayorIgual(Object r);
+}
